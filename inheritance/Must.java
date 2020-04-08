4458afd36d6dd1eade419b0bde5d667d7ffb764d
jhw510 <jhw510@hanmail.net>
2020-04-08 16:13:48 +0900

    200408

--- inheritance/Must.java
+++ inheritance/Must.java
@@ -8,42 +8,67 @@ public class Must {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		PhoneService phoneService = new PhoneService();
-		
-
+		String message= "";
 		while (true) {
 			System.out.println();
-			switch (JOptionPane.showInputDialog("0종료 \t 1.집전화 입력 \t 2.집전화 정보 출력"
-					+"\t 3.휴대전화 입력 \t 4.휴대전화출력")) {
-			case "0":return;
+			switch (JOptionPane.showInputDialog("0종료 \t 1.집전화 입력 \t "
+					+ "2.집전화 정보 출력" + 
+			"\t 3.휴대전화 입력 \t 4.휴대전화출력")) {
+			case "0":
+				return;
 
 			case "1":
 				for (int i = 0; i < 3; i++) {
-			
-					String[] values =JOptionPane.showInputDialog("전화번호 (예 :010-xxxx-xxxx) ,이름 ,통신사를 입력하시오").split(",");
-					
-					phoneService.addCelPhone(new Phone(values[0],values[1], values[2]));
-					
-				} 
+
+					String[] values = JOptionPane.showInputDialog("전화번호 (예:032-0000-0000),이름 ,"
+							+ "통신사를 입력하시오   ***,를 꼭 입력해주세요***").split(",");
+
+					phoneService.add(new Phone(values[0], values[1], values[2]));
+
+				}
+				break;
+			case "2":
+				Phone[] phones = phoneService.getPhones();
+				
+				for (int i = 0; i < 3; i++) {
+					Phone phone = phones[i];
+					message += String.format("출력 : %s, %s ,%s  \n",
+							phones[i].getPhoneNumber(), phones[i].getName(),
+							phones[i].getCompany());
+					/*
+					 * ( "번호: "+phones[i].getPhoneNumber()+" 이름: " + phones[i].getName() + " 통신사: "
+					 * +phones[i].getCompany());
+					 */
+				}
+				JOptionPane.showMessageDialog(null, message);
+				break;
+			case "3":
+				for (int i = 0; i < 3; i++) {
+
+					String[] values = JOptionPane.showInputDialog("전화번호 (예 :010-xxxx-xxxx) ,"
+							+ "이름 ,통신사를 입력하시오 ***,를 꼭 입력해주세요***").split(",");
+
+					phoneService.addCelPhone(new CelPhone(values[0], values[1], values[2],true));
+
+				}
+				break;
 				
 			
-				break;
-			case "2": 
-			Phone[] phones=phoneService.getPhones();
-			String message="";
-			for(int i=0; i<3; i++) {
-				Phone phone= phones[i];
-			message +=String.format("출력 : %s, %s ,%s  \n", phones[i].getPhoneNumber(),
-					phones[i].getName(),phones[i].getCompany());
-					/*(
-						"번호: "+phones[i].getPhoneNumber()+" 이름: " +  phones[i].getName() +
-						" 통신사: " +phones[i].getCompany());*/
-				} JOptionPane.showMessageDialog(null, message);
-			break;
-			case "3" :
+			case "4":
+				CelPhone[] celPhones = phoneService.getCelPhones();
+			
+				for (int i = 0; i < 3; i++) {
+					CelPhone celPhone = celPhones[i];
+					message += String.format("출력 : %s, %s ,%s  \n",
+							celPhones[i].getPhoneNumber(), celPhones[i].getName(),
+							celPhones[i].getCompany());
+					
+				}
+				JOptionPane.showMessageDialog(null, message);
 				
 				break;
-			case "4" : break;
 			}
 		}
 	}
 }
+
--- inheritance/PhoneService.java
+++ inheritance/PhoneService.java
@@ -1,38 +1,48 @@
 package com.jse.inheritance;
 
-
-
 public class PhoneService {
-private Phone[] phones;
-private int count;
-private CelPhone[] celPhones;
+	private Phone[] phones;
+	private int count;
+	private CelPhone[] celPhones;
 
-public PhoneService() {
-	phones = new Phone[3];
-	celPhones = new CelPhone[3]; 
-		count = 0;	
-}
-	public CelPhone[] getCelPhones() {
-	return celPhones;
-}
-public void setCelPhones(CelPhone[] celPhones) {
-	this.celPhones = celPhones;
-}
-	public void SetPhones(Phone[]phones) {
-		this.phones=phones;
+	public PhoneService() {
+		phones = new Phone[3];
+		celPhones = new CelPhone[3];
+		count = 0;
 	}
-	public  Phone[] getPhones() {
+
+	public CelPhone[] getCelPhones() {
+		return celPhones;
+	}
+
+	public void setCelPhones(CelPhone[] celPhones) {
+		this.celPhones = celPhones;
+	}
+
+	public void SetPhones(Phone[] phones) {
+		this.phones = phones;
+	}
+
+	public Phone[] getPhones() {
 		return phones;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
 	public int getCount() {
 		return count;
 	}
-	public void addCelPhone(Phone phone) {
+
+	public void add(Phone phone) {
 		phones[count] = phone;
-		
+
+		count++;
+	}
+	public void addCelPhone(CelPhone celPhone) {
+		celPhones[count] = celPhone;
+
 		count++;
 	}
 }
