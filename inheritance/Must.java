0f8c48449d7dfecbe804adf10615a383258adeac
jhw510 <jhw510@hanmail.net>
2020-04-08 15:30:46 +0900

    200408

--- inheritance/Must.java
+++ inheritance/Must.java
@@ -2,34 +2,43 @@ package com.jse.inheritance;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Must {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		PhoneService phoneService = new PhoneService();
 
 		while (true) {
-			System.out.println("0종료 1.전화번호정보 입력 2.전화번호 정보 출력");
-			switch (scanner.nextInt()) {
-			case 0:
-				System.out.println("종료");
+			System.out.println();
+			switch (JOptionPane.showInputDialog("0종료 1.전화번호정보 입력 2.전화번호 정보 출력")) {
+			case "0":
+				
 				return;
 
-			case 1:
-				System.out.println("1.전화번호 정보를 입력합니다");
+			case "1":
 				for (int i = 0; i < 3; i++) {
-					System.out.println("전화번호 (예 :010-xxxx-xxxx) ,이름 ,통신사를 입력하시오");
-					phoneService.add(new Phone(scanner.next(), scanner.next(), scanner.next()));
-
-				}
+			
+					String[] values =	JOptionPane.showInputDialog("전화번호 (예 :010-xxxx-xxxx) ,이름 ,통신사를 입력하시오").split(",");
+					
+					phoneService.add(new Phone(values[0],values[1], values[2]));
+					
+				} 
+				
+			
 				break;
-			case 2: System.out.println("2.전화번호 정보를 출력합니다");
+			case "2": 
 			Phone[] phones=phoneService.getPhones();
+			String message="";
 			for(int i=0; i<3; i++) {
 				Phone phone= phones[i];
-				System.out.println(
+			message +=String.format("출력 : %s, %s ,%s  \n", phones[i].getPhoneNumber(),
+					phones[i].getName(),phones[i].getCompany());
+					/*(
 						"번호: "+phones[i].getPhoneNumber()+" 이름: " +  phones[i].getName() +
-						" 통신사: " +phones[i].getCompany());
-				} break;
+						" 통신사: " +phones[i].getCompany());*/
+				} JOptionPane.showMessageDialog(null, message);
+			break;
 			}
 		}
 	}
